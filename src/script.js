a63d2775a94172acf4d6af57f4d0480925b87f52
Analysisbuilds <hardeynihyieq@gmail.com>
2026-09-19 01:00:17 +0100

    make /src root directory to aid vercel deployment

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import { newArrivalsProducts, topSellingProducts } from './src/Products.js';
+import { newArrivalsProducts, topSellingProducts } from '../src/Products.js';
 const closeAnnouncementBtn = document.getElementById('close-announcement');
 const announcementBar = document.getElementById('announcement-bar');
 const mobileMenuBtn = document.getElementById('mobile-menu-btn');
